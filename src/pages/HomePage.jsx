8bcb16d799d160133e69d7198659faa5eaf44780
ds-arunkumar <arunkumar519@gmail.com>
2025-03-15 09:41:32 +0530

    navbar added!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
-import { toast } from "react-toastify"
+import NavBar from "../components/NavBar";
+
 
 const HomePage = () => {
   return (
-    <div>
-      <button
-      className="btn btn-primary btn-lg btn-block border-0 "
-      onClick={()=>{
-        toast.success('Hello World!')
-      }}
-      >Toast</button>
-    </div>
+    <>
+      <NavBar />
+      <h1>HomePage</h1>
+    </>
   )
 }
 
