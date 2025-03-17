ef80f9e7be66623c090703031568ca1dc7d31756
ds-arunkumar <arunkumar519@gmail.com>
2025-03-17 10:40:47 +0530

    register page and login page design done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,21 @@ import NavBar from "../components/NavBar";
 const HomePage = () => {
   return (
     <>
-      <NavBar />
-      <h1>HomePage</h1>
+      <div className="container mt-5 text-center">
+        <h1 className="text-4xl text-gray-800">Welcome to Music!</h1>
+        <h4 className="text-xl text-gray-600">The best music platform</h4>
+      
+
+      <div className="max-w-2xl mx-auto mt-5 bg-white shadow-md rounded-md p-5 rounded-lg overflow-hidden border border-gray-200 ">
+        <h2 className="text-2xl text-gray-800">Features:</h2>
+        <ul className="mt-5 text-lg text-gray-600">
+          <li>Listen to your favorite music</li>
+          <li>Discover new music</li>
+          <li>Share your music with friends</li>
+          <li>And much more...</li>
+        </ul>
+      </div>
+    </div>
     </>
   )
 }
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+const RegisterPage = () =>{
+  return (
+    <div className="container mt-5 text-center">
+      <h1 className="text-4xl text-gray-800">Register</h1>
+      <div className="max-w-md mx-auto mt-5 bg-white shadow-md rounded-md p-5 rounded-lg overflow-hidden border border-gray-200">
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"            
+            />
+          </div> 
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <input type="password" id="password" name="password" placeholder="******" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"            
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button type="submit" className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Register</button>
+          </div>
+        </form>
+        <p className="mt-3 text-sm text-gray-600">Already have an account? <Link to="/login" className="text-indigo-600 hover:text-indigo-500">Login</Link></p>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterPage;
