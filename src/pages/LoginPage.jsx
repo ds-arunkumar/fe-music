655fbac69f7240405822fbdbcc097cbaa8205ae6
ds-arunkumar <arunkumar519@gmail.com>
2025-03-17 18:17:42 +0530

    login page flow done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,56 @@
-import { Link } from 'react-router-dom';
+import { selectEmail, selectPassword, setEmail, setPassword } from '../redux/features/auth/loginSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router';
+import authServices from '../services/authServices';
+import { toast } from 'react-toastify';
 const LoginPage = () => {
+
+  const email = useSelector(selectEmail);
+  const password = useSelector(selectPassword);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    
+    authServices.login({email, password})
+    .then((response)=>{
+      toast.success(response.data.message);
+
+      // clear the form
+      dispatch(setEmail(""));
+      dispatch(setPassword(""));
+
+      // navigate the user to the dashboard page
+      setTimeout(()=>{
+        navigate("/dashboard");
+      },500);
+    })
+    .catch((err)=>{
+      toast.error(err.response.data.message);
+    })
+  }
+
+
+
   return(
     <div className="container mt-5 text-center">
       <h1 className="text-4xl text-gray-800">Login</h1>
       <div className="max-w-md mx-auto mt-5 bg-white shadow-md rounded-md p-5 rounded-lg overflow-hidden border border-gray-200">
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
             <input type="email" id="email" name="email" placeholder="Email" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={email}
+            onChange={(e) => dispatch(setEmail(e.target.value))}
             />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
             <input type="password" id="password" name="password" placeholder="******" className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={password}
+            onChange={(e) => dispatch(setPassword(e.target.value))}
             /> 
           </div>
           <div className="flex items-center justify-between">
